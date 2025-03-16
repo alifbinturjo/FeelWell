0a326adf306bf08e345cad2a35578f3a798d855c
alifbinturjo <alif.turjo@northsouth.edu>
2025-03-16 22:31:29 +0600

    necessary fixes

--- app/src/main/java/com/example/feelwell/ResultActivity.java
+++ app/src/main/java/com/example/feelwell/ResultActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
 
-    private View verticalLine;
+    private View horizontalBar; // Changed from verticalLine to horizontalBar
     private View userScoreIndicator;
     private TextView severityLevelText;
 
@@ -20,7 +20,7 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         // Initialize views
-        verticalLine = findViewById(R.id.verticalLine);
+        horizontalBar = findViewById(R.id.horizontalBar); // Updated ID
         userScoreIndicator = findViewById(R.id.userScoreIndicator);
         severityLevelText = findViewById(R.id.severityLevelText);
 
@@ -34,27 +34,27 @@ public class ResultActivity extends AppCompatActivity {
         severityLevelText.setText("Severity Level: " + severityLevel);
 
         // Wait for the layout to be fully drawn before positioning the indicator
-        verticalLine.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        horizontalBar.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
                 // Remove the listener to prevent multiple calls
-                verticalLine.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                horizontalBar.getViewTreeObserver().removeOnGlobalLayoutListener(this);
 
-                // Plot the user's score on the vertical line
+                // Plot the user's score on the horizontal bar
                 plotUserScore(totalScore, userScore);
             }
         });
     }
 
     private void plotUserScore(int totalScore, int userScore) {
-        // Get the height of the vertical line
-        int verticalLineHeight = verticalLine.getHeight();
+        // Get the width of the horizontal bar
+        int horizontalBarWidth = horizontalBar.getWidth();
 
         // Calculate the position of the user's score indicator
-        float userScorePosition = ((float) userScore / totalScore) * verticalLineHeight;
+        float userScorePosition = ((float) userScore / totalScore) * horizontalBarWidth;
 
         // Set the position of the user's score indicator
-        userScoreIndicator.setY(verticalLine.getY() + verticalLineHeight - userScorePosition);
+        userScoreIndicator.setX(horizontalBar.getX() + userScorePosition);
 
         // Customize the user's score indicator
         userScoreIndicator.setBackgroundColor(Color.RED);
